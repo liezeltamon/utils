535133c426609950ea3597e29cc2dface01fe96b
liezeltamon <lizheltamon@gmail.com>
2023-11-17 02:32:15 +0000

    Add funx to get cellranger compatible fastq

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,5 @@
+require(R.utils)
+
 # No fix for levels of same size
 as_factor_bySize <- function(x, decreasing = TRUE){
   x <- as.character(x)
@@ -6,3 +8,26 @@ as_factor_bySize <- function(x, decreasing = TRUE){
   x <- factor(x, levels = ordered_levels)
   return(x)
 }
+
+# Rename fastq for cellranger count and friends (e.g. SRR14710616_S1_L001_R2_001.fastq.gz)
+## Illumina naming convention - https://support.illumina.com/help/BaseSpace_Sequence_Hub_OLH_009008_2/Source/Informatics/BS/NamingConvention_FASTQ-files-swBS.htm
+## - The lane number in the function is set to 1 (L001) as this is not often shown in fastq original file name 
+# src_dir = file.path("data-raw", "2023-10-06_scRNAseq")
+# out_dir = create_dir(file.path("data", "2023-10-06_scRNAseq_renamed"))
+# samp = "CO-NSC1"
+cellranger_renamefastq <- function(src_dir, out_dir, samples){
+  for(samp in samples){
+    
+    samp_paths <- list.files(src_dir, pattern = paste0("^", samp, "_R"))
+    samp_paths <- samp_paths[grepl("_001.fastq.gz$", samp_paths)]
+    
+    for(pth in samp_paths){
+      pth_renamed <- gsub(samp, paste0(samp, "_S1_L001"), pth)
+      R.utils::createLink(link = file.path(out_dir, pth_renamed),
+                          target = file.path(src_dir, pth))
+    }
+    
+  }
+}
+
+#rm(list=ls())
