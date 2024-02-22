f525b441528bab2ae061befe1914e5b9a955dc81
liezeltamon <lizheltamon@gmail.com>
2024-02-22 11:52:33 +0000

    Add microglia state markers

---
+++ R/marker_microglia.R
@@ -0,0 +1,7 @@
+homeostatic = c("P2RY12", "TMEM119",
+                "LAG3", "IRF4") # Used in Vahsen et al. 2022 Sci Rep Coculture of microglia and motor neurons
+dam_early = c("TYROBP", "APOE")
+dam_late = c("CLEC7A", "SPP1")
+
+# Vahsen et al. 2022 Sci Rep Coculture of microglia and motor neurons
+microglia_vs_blood_monocytes = c("P2RY12", "MERTK", "TREM2", "TMEM119", "PROS1", "C1QA", "GAS6", "PR34")
