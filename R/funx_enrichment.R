40e82a5b18ad3ae930fd473cf50163edf6c080b3
liezeltamon <lizheltamon@gmail.com>
2024-07-24 17:48:39 +0100

    Use bitr for conversion of gene key types, add steps to remove invalid genes

--- R/funx_enrichment.R
+++ R/funx_enrichment.R
@@ -5,159 +5,157 @@
 ################################################################################
 # LIBRARIES & DEPENDENCIES * LIBRARIES & DEPENDENCIES * LIBRARIES & DEPENDENCIES 
 ################################################################################
+# library(bitr)
+# library(DOSE) # for setReadable()
 # library(org.Hs.eg.db)
-# BiocManager::install("clusterProfiler"); library(clusterProfiler)
-# clusterProfiler v3.8.1  For help: 
-# https://yulab-smu.github.io/clusterProfiler-book/chapter1.html
-# https://guangchuangyu.github.io/software/clusterProfiler
+# For help:
+# https://yulab-smu.top/biomedical-knowledge-mining-book/clusterprofiler-go.html
 # Citation:
 #  Guangchuang Yu, Li-Gen Wang, Yanyan Han, Qing-Yu He. 
 #  clusterProfiler: an R package for comparing biological themes among gene clusters. 
 #  OMICS: A Journal of Integrative Biology. 2012, 16(5):284-287.
 ### FUNCTION ###################################################################
 funxAnno <- function(input = list(foreground, background),
+                     org_db = org.Hs.eg.db,
                      org = org.Hs.eg.db, # org.Hs.eg.db (GO, human) | "hsa" (KEGG, human)
                      # c("SYMBOL", "kegg", "ncbi-geneid", "ncib-proteinid", "uniprot")
                      inputKey = "SYMBOL",
-                     approach = "GO_BP",
+                     useKey = "ENTREZID",
+                     approach = "GO_BP", # KEGG, MKEGG, GO_BP, GO_CC, GO_MF, GO_ALL
                      # For saving GO or KEGG table; if NULL, table not saved
-                     filePath = "lib/ekegg.txt"
+                     filePath = NULL,
+                     ... # Common parameters between enrichG0() and enrichKEGG()
 ){
   
-  appro <- gsub(x=approach, pattern="GO_", replacement="", fixed=TRUE)
-  accepted.KEGG <- c("kegg", "ncbi-geneid", "ncbi-proteinid", "uniprot")
-  accepted.BP <- accepted.CC <- accepted.MF <- accepted.ALL <- c("SYMBOL", "ncbi-geneid") 
-  eval(parse(text=paste0(
-    "acceptedKeys <- accepted.", appro
-  )))
-  
-  if(!inputKey%in%acceptedKeys){
-    stop("Key type not accepted by", approach, ". Choose among ",
-         paste(acceptedKeys, collapse=", "), ".")
+  # enrichKEGG() accepts one of "kegg", 'ncbi-geneid', 'ncbi-proteinid' and 'uniprot'
+  # Use "ENTREZID" by default so downstream checks applicable and equivalent with when using enrichGO()
+  if (approach == "KEGG"){
+    message("funxAnno(): Approach is ", approach, ". Using ENTREZID key type by default.")
+    useKey <- "ENTREZID"
   }
   
-  # Clean gene lists
-  input <- lapply(X=input, FUN=function(genes){
-    unique(genes[!is.na(genes) & genes!="NA" & genes!="" & genes!=" "])
+  # Preprocessing
+  
+  orgdb_keytypes <- columns(org.Hs.eg.db)
+  if (!any(c(inputKey, useKey) %in% orgdb_keytypes)) {
+    
+    message("funxAnno(): Available OrgDb key types: \n")
+    cat(orgdb_keytypes)
+    stop("funxAnno(): Invalid inputKey and/or useKey")
+    
+  }
+  
+  # Clean input gene lists
+  input_universe <- keys(org.Hs.eg.db, keytype = inputKey)
+  input_cleaned <- lapply(input, FUN = function (genes) {
+    unique(genes[genes %in% input_universe])
   })
   
-  if(approach=="KEGG"){
+  message("Number of genes after removing duplicates and inputs not in OrgDb: \n")
+  print(
+    rbind(orig = lengths(input),
+          cleaned = lengths(input_cleaned),
+          `orig - cleaned` = lengths(input) - lengths(input_cleaned))
+  )
+  
+  # (If required) Convert key type
+  if (!is.null(useKey) & !identical(inputKey, useKey)) {
     
-    e.out <- enrichKEGG(gene=input[[1]], organism=org, keyType=inputKey,
-                        # clusterProfiler uses the same adjustment methods 
-                        # as the R function stats::p.adjust 
-                        # The "BH" (aka "fdr") and "BY" method of Benjamini, Hochberg, 
-                        # and Yekutieli control the false discovery rate, the expected 
-                        # proportion of false discoveries amongst the rejected hypotheses. 
-                        # The false discovery rate is a less stringent condition than 
-                        # the family-wise error rate, so these methods are more powerful 
-                        # than the others.
-                        pvalueCutoff=0.05, pAdjustMethod="BH", universe=input[[2]],
-                        # Gene set size limits for the GO terms to be considered (DEF).
-                        # This was added so that parent terms like "biological process"
-                        # won't be tested. 
-                        minGSSize=10, maxGSSize=500, qvalueCutoff=0.2,
-                        #If FALSE, download the latest KEGG data; If TRUE, use KEGG.db
-                        use_internal_data=FALSE)
+    message("funxAnno(): Converting key type")
+    conversion_df <- bitr(unique(unlist(input)), fromType = inputKey, toType = useKey, 
+                          OrgDb = org.Hs.eg.db, drop = TRUE)
+    useInput <- lapply(input, FUN = function (genes) {
+      unique(conversion_df[conversion_df[[inputKey]] %in% genes, useKey])
+    })
     
-  } else if( approach%in%c("GO_BP", "GO_CC", "GO_MF", "GO_ALL") ){
-    
-    e.out <- enrichGO(gene=input[[1]], OrgDb=org, 
-                      keyType=ifelse(inputKey=="ncbi-geneid", "ENTREZID", inputKey), 
-                      ont=appro, pvalueCutoff=0.05, pAdjustMethod="BH", 
-                      universe=input[[2]], minGSSize=10, maxGSSize=500,
-                      # Whether mapping gene ID to gene Name
-                      readable=ifelse(inputKey=="ncbi-geneid", TRUE, FALSE),
-                      # If ont='ALL', whether pool 3 GO sub-ontologies
-                      pool=ifelse(approach=="GO_ALL", TRUE, FALSE))
-    
+    message("Number of genes after conversion: \n")
+    print(
+      rbind(orig = lengths(input),
+            cleaned = lengths(input_cleaned),
+            converted = lengths(useInput),
+            `cleaned - converted` = lengths(input_cleaned) - lengths(useInput))
+    )
+      
   } else {
-    stop("Invalid approach supplied. Choices are GO_BP, GO_CC, GO_MF, GO_ALL and KEGG.")
+    useKey <- inputKey
+    useInput <- input
   }
   
-  e.out <- data.frame(e.out, stringsAsFactors=FALSE, row.names=NULL)
+  # ifelse(inputKey == "ncbi-geneid", "ENTREZID", inputKey)
+  
+  # ORA
+  
+  if (approach %in% c("KEGG", "MKEGG")) {
+    
+    if (approach == "KEGG") {
+      
+      message("funxAnno(): Running enrichKEGG()...")
+      e_out <- enrichKEGG(gene = useInput[[1]], organism = org, keyType = "ncbi-geneid",
+                          #pvalueCutoff = 0.05, pAdjustMethod = "BH",
+                          universe = useInput[[2]],
+                          #minGSSize = 10, maxGSSize = 500,
+                          #If FALSE, download the latest KEGG data; If TRUE, use KEGG.db
+                          use_internal_data = FALSE,
+                          ...)
+      
+    } else if (approach == "MKEGG") {
+      
+      message("funxAnno(): Running enrichMKEGG()...")
+      e_out <- enrichMKEGG(gene = useInput[[1]], organism = org, keyType = "ncbi-geneid",
+                           universe = useInput[[2]], ...)
+      
+    }
+    
+    e_out <- DOSE::setReadable(e_out, org_db, keyType = "ENTREZID")
+    
+  } else if (approach %in% c("GO_BP", "GO_CC", "GO_MF", "GO_ALL")) {
+    
+    appro <- gsub("GO_", "", approach, fixed = TRUE)
+    e_out <- enrichGO(gene = useInput[[1]], OrgDb = org_db, keyType = useKey, 
+                      ont = appro,
+                      # clusterProfiler uses the same adjustment methods 
+                      # as the R function stats::p.adjust 
+                      # The "BH" (aka "fdr") and "BY" method of Benjamini, Hochberg, 
+                      # and Yekutieli control the false discovery rate, the expected 
+                      # proportion of false discoveries amongst the rejected hypotheses. 
+                      # The false discovery rate is a less stringent condition than 
+                      # the family-wise error rate, so these methods are more powerful 
+                      # than the others.
+                      #pvalueCutoff = 0.05, pAdjustMethod = "BH", 
+                      universe = useInput[[2]],
+                      # Gene set size limits for the GO terms to be considered (DEF).
+                      # This was added so that parent terms like "biological process"
+                      # won't be tested.
+                      #minGSSize = 10, maxGSSize = 500,
+                      # Whether mapping gene ID to gene Name
+                      readable = ifelse(useKey != "SYMBOL", TRUE, FALSE),
+                      # If ont='ALL', whether pool 3 GO sub-ontologies
+                      pool = ifelse(approach == "GO_ALL", TRUE, FALSE),
+                      ...)
+    
+  } else {
+    stop("funxAnnot(): Invalid approach supplied. Choices are GO_BP, GO_CC, GO_MF, GO_ALL and KEGG.
+         Can extend code to allow other databases supported by clusterProfiler.")
+  }
+  
+  e_out <- data.frame(e_out, stringsAsFactors = FALSE, row.names = NULL)
   # To avoid separation of description names when opening the csv file.
-  e.out$Description <- gsub(x=e.out$Description, pattern=",", replacement=" ",  
-                            fixed=TRUE)
-  if(approach!="GO_ALL" & nrow(e.out)>0){
-    e.out <- cbind.data.frame(ONTOLOGY=approach, e.out, stringsAsFactors=FALSE)
-  } else if(approach=="GO_ALL" & nrow(e.out)>0){
-    e.out$ONTOLOGY <- paste("GO_", e.out$ONTOLOGY, sep="")
+  e_out$Description <- gsub(",", " ", e_out$Description, fixed = TRUE)
+  if (approach != "GO_ALL" & nrow(e_out) > 0) {
+    e_out <- cbind.data.frame(ONTOLOGY = approach, e_out, stringsAsFactors = FALSE)
+  } else if (approach == "GO_ALL" & nrow(e_out) > 0) {
+    e_out$ONTOLOGY <- paste("GO_", e_out$ONTOLOGY, sep = "")
   }
   
   # Save table
   if( !is.null(filePath) ){
-    write.csv(x=e.out, file=filePath, row.names=FALSE, quote=FALSE)
+    write.csv(e_out, file = filePath, row.names = FALSE, quote = FALSE)
   }
   
-  return(e.out)
+  return(e_out)
   
 }
-################################################################################
 
-################################################################################
 # Combine GO_ALL and KEGG enrichment analyses for a set of genes in one 
 # dataframe.
-################################################################################
-# LIBRARIES & DEPENDENCIES * LIBRARIES & DEPENDENCIES * LIBRARIES & DEPENDENCIES 
-################################################################################
-# source(paste0(lib, "/funxAnno.R"))
-# library(org.Hs.eg.db)
-# library(clusterProfiler)
-### FUNCTION ###################################################################
-funxAnnoWrapper <- function(input = 'list(HUGO foreground genes, HUGO background genes)',
-                            filePath = 'dir for saving individual GO and KEGG tables; 
-                                        if NULL, tables not saved',
-                            hugoEntrez.file = 'Conversion table of HUGO gene 
-                                               symbols to ncbi-geneid for KEGG analysis'
-){
-  
-  # Read conversion table for ncbi-geneid to HUGO symbols for output
-  HE.conv <- read.delim(file=hugoEntrez.file, header=T, row.names=NULL, stringsAsFactors=F)
-  HE.conv <- HE.conv[!is.na(HE.conv$SYMBOL) & !is.na(HE.conv$ENTREZID),]
-  HE.conv$ENTREZID <- as.character(HE.conv$ENTREZID)
-  
-  # Gene sets
-  GENE <- list()
-  
-  GENE$GO_ALL <- input
-  GENE$GO_ALL <- lapply(X=GENE$GO_ALL, FUN=function(set){
-    return( set[!is.na(set)] )
-  }) 
-  
-  GENE$KEGG <- lapply(X=GENE$GO_ALL, FUN=function(set){
-    set <- HE.conv$ENTREZID[ HE.conv$SYMBOL%in%set ]
-    return( set[!is.na(set)] )
-  })
-  
-  # Enrichment analysis
-  funxAnnoOut <- sapply(X=names(GENE), simplify=F, FUN=function(app){
-    
-    chunk <- funxAnno(input=GENE[[app]],
-                      org=ifelse(app=="GO_ALL", "org.Hs.eg.db", "hsa"), 
-                      inputKey=ifelse(app=="GO_ALL", "SYMBOL", "ncbi-geneid"), 
-                      approach=app, filePath=filePath)
-    return(chunk)
-    
-  })
-  
-  funxAnnoOut <- do.call("rbind", funxAnnoOut)
-  rownames(funxAnnoOut) <- NULL
-  
-  rm(GENE)
-  
-  # Convert ncbi geneids to HUGO
-  KEGG.TF <- funxAnnoOut$ONTOLOGY=="KEGG"
-  if( sum(KEGG.TF)>0 ){
-    funxAnnoOut[KEGG.TF,"geneID"] <- sapply(X=funxAnnoOut[KEGG.TF, "geneID"], 
-                                            simplify=TRUE, FUN=function(id){
-                                              id <- unique(strsplit(x=id, split="/", fixed=TRUE)[[1]])
-                                              paste(x=HE.conv[HE.conv$ENTREZID%in%id,"SYMBOL"],
-                                                    collapse="/")
-                                            })
-  }
-  
-  return(funxAnnoOut)
-  
-} 
-################################################################################
+#bind_rows(kegg = kegg_out, go = go_out, .id = "source")
