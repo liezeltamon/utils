6c4fb5df20b1de5699edea6f824312496f6b47bb
liezeltamon <lizheltamon@gmail.com>
2024-09-01 18:41:09 +0100

    Add collection of colours highly different

--- R/plot.R
+++ R/plot.R
@@ -5,4 +5,22 @@ edges <- as.data.frame(table(as.data.frame(is_geqthreshold_mx)))
     edges[[i]] <- as.character(edges[[i]])
   }
   SankeyDiagram(edges[, -(criteria_len)], link.color = "Source", weights = edges$Freq, max.categories = 23, hovertext.show.percentages = TRUE, label.show.counts = TRUE, font.size = 10, variables.share.values = TRUE, label.show.percentages = TRUE)
-  
+
+# Collection of colours mostly distinguishable  
+# From https://stackoverflow.com/questions/9563711/r-color-palettes-for-many-data-classes
+c25 <- c(
+  "dodgerblue2", "#E31A1C", # red
+  "green4",
+  "#6A3D9A", # purple
+  "#FF7F00", # orange
+  "black", "gold1",
+  "skyblue2", "#FB9A99", # lt pink
+  "palegreen2",
+  "#CAB2D6", # lt purple
+  "#FDBF6F", # lt orange
+  "gray70", "khaki2",
+  "maroon", "orchid1", "deeppink1", "blue1", "steelblue4",
+  "darkturquoise", "green1", "yellow4", "yellow3",
+  "darkorange4", "brown"
+)
+pie(rep(1, 25), col = c25)
